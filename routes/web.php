da2f6229d745c238bd435b4c7aa9b9205af8e357
Aeyma Syahira <241230023@unmuhpnk.ac.id>
2026-06-16 17:42:32 +0700

    fix: gabungkan rute home, program, dan transparansi

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-// Import Controller Transparansi di bagian atas
 use App\Http\Controllers\TransparansiController;
 
 // 1. Rute Utama (Halaman Beranda / Home)
@@ -11,15 +10,11 @@ Route::get('/', function () {
     return view('home', compact('programs'));
 })->name('home');
 
-
-// 2. Rute untuk Halaman Katalog Program Donasi (Versi Flat Tanpa Folder)
+// 2. Rute untuk Halaman Katalog Program Donasi
 Route::get('/program', function () {
     $programs = DB::table('programs')->get();
-    
-    // Langsung mengarah ke file program.blade.php
     return view('program', compact('programs'));
 })->name('program.index');
 
-
-// 3. Rute untuk Halaman Transparansi Dana Donasi (Menggunakan Controller)
+// 3. Rute untuk Halaman Transparansi Dana Donasi
 Route::get('/transparansi', [TransparansiController::class, 'index'])->name('transparansi.index');
