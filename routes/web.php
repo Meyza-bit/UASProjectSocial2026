1b72e404273a509534d273cf485ba5e7c1be7878
Meyza-bit <241230045@unmuhpnk.ac.id>
2026-06-21 22:31:15 +0700

    fix: tambah model ProgramDonasi dan benerin conflict app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\DonasiController;
-=======
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProgramController;
@@ -12,7 +8,6 @@ use App\Http\Controllers\DonasiController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\TransparansiController;
->>>>>>> main
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -22,36 +17,6 @@ Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
-<<<<<<< HEAD
-
-// 2. Halaman Katalog Daftar Kebutuhan Barang kamu
-Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
-
-// 3. Halaman Form Input Pengiriman Barang (Buatan PM)
-Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
-
-// 4. Handler untuk memproses dan menyimpan data dari form saat disubmit
-Route::post('/barang/store', [BarangController::class, 'store'])->name('barang.store');
-
-// ===== Modul Donasi =====
-Route::get('/donasi/create', [DonasiController::class, 'create'])->name('donasi.create');
-
-Route::get('/donasi/pembayaran', [DonasiController::class, 'pembayaran'])->name('donasi.pembayaran');
-
-// Rute untuk memproses data dari form donasi (POST)
-Route::post('/donasi/store', [DonasiController::class, 'store'])->name('donasi.store');
-
-Route::post('/donasi/konfirmasi', [DonasiController::class, 'konfirmasi'])->name('donasi.konfirmasi');
-
-// FIX: typo "pembayran" diperbaiki jadi "pembayaran-instruksi"
-Route::get('/donasi/pembayaran-instruksi', [DonasiController::class, 'instruksi'])->name('donasi.instruksi');
-
-// RUTE BARU: dipanggil saat user klik "Saya Sudah Melakukan Pembayaran"
-Route::post('/donasi/selesai', [DonasiController::class, 'selesai'])->name('donasi.selesai');
-
-// RUTE BARU: halaman terima kasih setelah bukti pembayaran berhasil diupload
-Route::get('/donasi/terimakasih/{id}', [DonasiController::class, 'terimakasih'])->name('donasi.terimakasih');
-=======
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::get('/program', [ProgramController::class, 'index'])->name('program.index');
@@ -64,4 +29,3 @@ Route::post('/barang', [BarangController::class, 'store'])->name('barang.store')
 Route::get('/feedback', [FeedbackController::class, 'index'])->name('feedback.index');
 Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.store')->middleware('auth');
 Route::get('/transparansi', [TransparansiController::class, 'index'])->name('transparansi');
->>>>>>> main
