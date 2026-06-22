6f67cd28a707d2c50bc165a08c73ac2895574184
sayanfiqi <syafiqindirwan@gmail.com>
2026-06-22 15:01:36 +0700

    Fix conflict DatabaseSeeder halaman feedback

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User dummy
